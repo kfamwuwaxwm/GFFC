d4ea141bf0c1aa36708e46d1d844dcb03362d519
ENNP <74237836+ENNP@users.noreply.github.com>
2021-03-05 14:44:41 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,87 @@
+<br>
+
+# GFFC
+Github Followers Following Check
+
+<br>
+
+#### [ 1 / 5 ] Program Introduction
+- You can Check FFF [ Follow For Follow ] in the GitHub.
+- `FFF == Follow For Follow == Follow each other`
+
+<br>
+
+#### [ 2 / 5 ] LICENSE
+- [MIT License](https://github.com/ENNP/GFFC/blob/main/LICENSE)
+
+<br>
+
+#### [ 3 / 5 ] Please Check class before using it.
+- After months of monitoring the GitHub update, it was confirmed that the class changed periodically.
+- If changed, only the variables [span_v1 and span_v2](https://github.com/ENNP/GFFC/blob/main/collecting_GFFC.py#L18) need to be changed.
+
+<br>
+
+- [ Span_v1 ]
+
+![](https://github.com/ENNP/GFFC/blob/main/img/3.png)
+
+<br>
+
+![](https://github.com/ENNP/GFFC/blob/main/img/4.png)
+
+<br><br>
+
+- [ Span_v2 ]
+
+![](https://github.com/ENNP/GFFC/blob/main/img/1.png)
+
+<br>
+
+![](https://github.com/ENNP/GFFC/blob/main/img/2.png)
+
+<br>
+
+
+
+#### [ 4 / 5 ] How to run
+- First
+  - Python collecting_GFFC.py
+
+<br>
+
+- Second
+  - Python analysis_GFFC.py
+
+<br>
+
+#### [ 5 / 5 ] Execution Results
+- Python collecting_GFFC.py
+
+![](https://github.com/ENNP/GFFC/blob/main/img/5.png)
+
+<br>
+
+- Python analysis_GFFC.py ( yes = FFF , [ no ] = No FFF ) / `FFF == Follow For Follow == Follow each other`
+```
+  [ 1 ] Page | https://github.com/ennp?tab=following&page=1
+    yes Github_id
+    yes Github_id
+    [ no ] Github_id
+    yes Github_id
+    [ no ] Github_id
+    yes Github_id
+    [ no ] Github_id
+    [ no ] Github_id
+    
+[ 2 ] Page | https://github.com/ennp?tab=following&page=2
+    yes Github_id
+    [ no ] Github_id
+    yes Github_id
+    yes Github_id
+    yes Github_id
+    yes Github_id
+    yes Github_id
+    [ no ] Github_id
+    [ no ] Github_id
+```
---
+++ analysis_GFFC.py
@@ -0,0 +1,123 @@
+import os
+
+
+
+def user_id():
+
+	p_user_id = open("./result_GFFC/id.db","r")
+	user_id = p_user_id.read()
+	p_user_id.close()
+
+	return user_id
+
+
+
+def count_all(user_id):
+
+	count_all_pwd = "./result_GFFC/" + user_id + "-count.db"
+
+	p_count_all = open(count_all_pwd,"r")
+	count_all = p_count_all.read()
+	p_count_all.close()
+
+	count_all = count_all.split(",")
+
+	count_followers = count_all[0]
+	count_following = count_all[1]
+
+	return int(count_followers), int(count_following)
+
+
+
+
+
+def check_my_person(user_id, count_following, count_followers):
+
+
+	check_followers_name = "./result_GFFC/" + user_id + "-followers.db"
+	check_following_name = "./result_GFFC/" + user_id + "-following.db"
+
+	p_followers_name = open(check_followers_name,"r")
+	p_following_name = open(check_following_name,"r")
+
+
+	following_array = []
+
+	for i in range(0,count_following + 1):
+
+		following_array.append(p_following_name.readline()[4:].replace("\n",""))
+
+	p_following_name.close()
+
+	del following_array[0]
+
+
+
+
+	followers_array = []
+
+	for i in range(0,count_followers + 1):
+
+		followers_array.append(p_followers_name.readline()[4:].replace("\n",""))
+
+	p_followers_name.close()
+
+	del followers_array[0]
+
+
+
+	following_person = 1
+	page = 1
+	result_print = "\n\n\n  [ " + str(page) + " ] Page | https://github.com/" + user_id + "?tab=following&page=" + str(page) + "\n"
+	for following_name in following_array:
+
+		if following_person == 51:
+
+			following_person = 1
+			page += 1
+			result_print += "\n\n\n  [ " + str(page) + " ] Page | https://github.com/" + user_id + "?tab=following&page=" + str(page) + "\n"
+
+
+		else:
+			pass
+
+
+		for followers_array_2 in followers_array:
+
+			if following_name == followers_array_2:
+
+				result_print += "    yes " + following_name + "\n"
+				break
+			else:
+
+				result_print += "    [ no ] " + following_name + "\n"
+				break
+
+
+		following_person += 1
+
+
+
+	result_print += "\n\n\n"
+
+	os.system("rm -rf ./result_GFFC/" + user_id + "-result.db")
+	os.system("touch ./result_GFFC/" + user_id + "-result.db")
+
+	pwd = "./result_GFFC/" + user_id + "-result.db"
+	f = open(pwd,"w")
+	f.write(result_print)
+	f.close()
+
+	print "\n\n    [ Done ] Check the File -> ./result_GFFC/" + user_id + "-result.db\n\n"
+
+
+
+
+if __name__ == "__main__":
+
+	user_id = user_id()
+
+	count_followers, count_following = count_all(user_id)
+
+	result_print = check_my_person(user_id, count_following, count_followers)
+
